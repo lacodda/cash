726f523d90dff1909b3294881283ed59144fda5b
Kirill Lahtachev <lahtachev@gmail.com>
2021-09-16 12:26:19 +0400

    add element-plus, vue-i18n

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,83 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <el-container>
+    <el-header>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1">Processing Center</el-menu-item>
+        <el-sub-menu index="2">
+          <template #title>Workspace</template>
+          <el-menu-item index="2-1">item one</el-menu-item>
+          <el-menu-item index="2-2">item two</el-menu-item>
+          <el-menu-item index="2-3">item three</el-menu-item>
+          <el-sub-menu index="2-4">
+            <template #title>item four</template>
+            <el-menu-item index="2-4-1">item one</el-menu-item>
+            <el-menu-item index="2-4-2">item two</el-menu-item>
+            <el-menu-item index="2-4-3">item three</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="3" disabled>Info</el-menu-item>
+        <el-menu-item index="4">Orders</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>Main</el-main>
+  </el-container>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import {
+  ElMenuItem,
+  ElContainer,
+  ElHeader,
+  ElMain,
+  ElMenu,
+  ElSubMenu,
+  ElButton,
+} from "element-plus";
+
+export default defineComponent({
+  components: {
+    ElMenuItem,
+    ElContainer,
+    ElHeader,
+    ElMain,
+    ElMenu,
+    ElSubMenu,
+    ElButton,
+  },
+  setup() {
+    const activeIndex = ref("1");
+    const activeIndex2 = ref("1");
+    const handleSelect = (key, keyPath) => {
+      console.log(key, keyPath);
+    };
+    return {
+      activeIndex,
+      activeIndex2,
+      handleSelect,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
   color: #2c3e50;
-  margin-top: 60px;
+  font-weight: 400;
+  -webkit-font-smoothing: antialiased;
+  -webkit-tap-highlight-color: transparent;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
 }
 </style>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,41 @@
+const { join, resolve } = require('path');
+const { readFileSync } = require('fs');
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-const { join } = require('path');
-const { readFileSync } = require('fs');
+import elementPlus from 'vite-plugin-element-plus';
+import i18n from '@intlify/vite-plugin-vue-i18n';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    elementPlus({
+      // if you need to use the *.scss source file, you need to uncomment this comment
+      // useSource: true
+      // format: mode === 'development' ? 'esm' : 'cjs',
+    }),
+    i18n({
+      // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+      // compositionOnly: false,
+
+      // you need to set i18n resource including paths!
+      include: resolve(__dirname, './i18n/**'),
+    }),
+  ],
   server: {
     https: {
       key: readFileSync(join(__dirname, '../ssl/ssl.key')),
       cert: readFileSync(join(__dirname, '../ssl/ssl.crt')),
     },
     port: 3080,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // example : additionalData: `@import "./src/design/styles/variables";`
+        // dont need include file extend .scss
+        additionalData: `@import "./src/scss/element-plus";`,
+      },
+    },
   },
 });
