8da4bed35fafcbf092cdab6f9a791ae8801dd96e
Kirill Lahtachev <lahtachev@gmail.com>
2021-10-25 15:27:24 +0400

    add "timeSum" in WorkTimeService

--- server/src/statistics/statistics.service.ts
+++ server/src/statistics/statistics.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import * as $R from 'ramda';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { RangeDto } from '../dto';
 
 import { WorkTimeService } from '../work-time/work-time.service';
@@ -8,7 +9,15 @@ export class StatisticsService {
   constructor(private readonly workTimeService: WorkTimeService) {}
 
   async getWorkTimeStatistics(dto: RangeDto) {
-    const storages = await this.workTimeService.findAll(dto);
-    return storages;
+    try {
+      const getTimeSum = $R.pipe($R.map($R.prop('time')), $R.sum);
+      const { data } = await this.workTimeService.findAll(dto);
+      const timeSum = getTimeSum(data);
+      return {
+        timeSum,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 }
